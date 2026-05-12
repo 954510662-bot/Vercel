7a8143a1eba0f6bc41a393292d042bb7e16ab397
CodeBrush Developer <developer@codebrush.com>
2026-05-12 18:44:20 +0800

    fix: 删除vercel.json，让Vercel自动检测

--- server/src/websocket/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './routes';
-export { setupWebSocket } from './websocket';
-export { collaborationManager } from './websocket/collaboration';
-export { presenceManager } from './websocket/presence';
